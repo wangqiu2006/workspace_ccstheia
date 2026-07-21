cf53c4f3a618bffc0f72061c6f63654a18a7f3ea
wangqiu2006 <wangqiu2006@users.noreply.github.com>
2026-07-21 13:52:23 +0800

    2025

---
+++ 2025xiaochexunji/Debug/ti_msp_dl_config.c
@@ -0,0 +1,411 @@
+/*
+ * Copyright (c) 2023, Texas Instruments Incorporated
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * *  Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * *  Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * *  Neither the name of Texas Instruments Incorporated nor the names of
+ *    its contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+ * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/*
+ *  ============ ti_msp_dl_config.c =============
+ *  Configured MSPM0 DriverLib module definitions
+ *
+ *  DO NOT EDIT - This file is generated for the MSPM0G350X
+ *  by the SysConfig tool.
+ */
+
+#include "ti_msp_dl_config.h"
+
+DL_TimerA_backupConfig gPWM_RIGHTBackup;
+DL_TimerA_backupConfig gPWM_LEFTBackup;
+
+/*
+ *  ======== SYSCFG_DL_init ========
+ *  Perform any initialization needed before using any board APIs
+ */
+SYSCONFIG_WEAK void SYSCFG_DL_init(void)
+{
+    SYSCFG_DL_initPower();
+    SYSCFG_DL_GPIO_init();
+    /* Module-Specific Initializations*/
+    SYSCFG_DL_SYSCTL_init();
+    SYSCFG_DL_PWM_RIGHT_init();
+    SYSCFG_DL_PWM_LEFT_init();
+    SYSCFG_DL_TIMER_0_init();
+    SYSCFG_DL_UART_GYRO_init();
+    SYSCFG_DL_UART_BT_init();
+    /* Ensure backup structures have no valid state */
+	gPWM_RIGHTBackup.backupRdy 	= false;
+	gPWM_LEFTBackup.backupRdy 	= false;
+
+
+
+}
+/*
+ * User should take care to save and restore register configuration in application.
+ * See Retention Configuration section for more details.
+ */
+SYSCONFIG_WEAK bool SYSCFG_DL_saveConfiguration(void)
+{
+    bool retStatus = true;
+
+	retStatus &= DL_TimerA_saveConfiguration(PWM_RIGHT_INST, &gPWM_RIGHTBackup);
+	retStatus &= DL_TimerA_saveConfiguration(PWM_LEFT_INST, &gPWM_LEFTBackup);
+
+    return retStatus;
+}
+
+
+SYSCONFIG_WEAK bool SYSCFG_DL_restoreConfiguration(void)
+{
+    bool retStatus = true;
+
+	retStatus &= DL_TimerA_restoreConfiguration(PWM_RIGHT_INST, &gPWM_RIGHTBackup, false);
+	retStatus &= DL_TimerA_restoreConfiguration(PWM_LEFT_INST, &gPWM_LEFTBackup, false);
+
+    return retStatus;
+}
+
+SYSCONFIG_WEAK void SYSCFG_DL_initPower(void)
+{
+    DL_GPIO_reset(GPIOA);
+    DL_GPIO_reset(GPIOB);
+    DL_TimerA_reset(PWM_RIGHT_INST);
+    DL_TimerA_reset(PWM_LEFT_INST);
+    DL_TimerG_reset(TIMER_0_INST);
+    DL_UART_Main_reset(UART_GYRO_INST);
+    DL_UART_Main_reset(UART_BT_INST);
+
+    DL_GPIO_enablePower(GPIOA);
+    DL_GPIO_enablePower(GPIOB);
+    DL_TimerA_enablePower(PWM_RIGHT_INST);
+    DL_TimerA_enablePower(PWM_LEFT_INST);
+    DL_TimerG_enablePower(TIMER_0_INST);
+    DL_UART_Main_enablePower(UART_GYRO_INST);
+    DL_UART_Main_enablePower(UART_BT_INST);
+    delay_cycles(POWER_STARTUP_DELAY);
+}
+
+SYSCONFIG_WEAK void SYSCFG_DL_GPIO_init(void)
+{
+
+    DL_GPIO_initPeripheralOutputFunction(GPIO_PWM_RIGHT_C0_IOMUX,GPIO_PWM_RIGHT_C0_IOMUX_FUNC);
+    DL_GPIO_enableOutput(GPIO_PWM_RIGHT_C0_PORT, GPIO_PWM_RIGHT_C0_PIN);
+    DL_GPIO_initPeripheralOutputFunction(GPIO_PWM_RIGHT_C1_IOMUX,GPIO_PWM_RIGHT_C1_IOMUX_FUNC);
+    DL_GPIO_enableOutput(GPIO_PWM_RIGHT_C1_PORT, GPIO_PWM_RIGHT_C1_PIN);
+    DL_GPIO_initPeripheralOutputFunction(GPIO_PWM_LEFT_C0_IOMUX,GPIO_PWM_LEFT_C0_IOMUX_FUNC);
+    DL_GPIO_enableOutput(GPIO_PWM_LEFT_C0_PORT, GPIO_PWM_LEFT_C0_PIN);
+    DL_GPIO_initPeripheralOutputFunction(GPIO_PWM_LEFT_C1_IOMUX,GPIO_PWM_LEFT_C1_IOMUX_FUNC);
+    DL_GPIO_enableOutput(GPIO_PWM_LEFT_C1_PORT, GPIO_PWM_LEFT_C1_PIN);
+
+    DL_GPIO_initPeripheralOutputFunction(
+        GPIO_UART_GYRO_IOMUX_TX, GPIO_UART_GYRO_IOMUX_TX_FUNC);
+    DL_GPIO_initPeripheralInputFunction(
+        GPIO_UART_GYRO_IOMUX_RX, GPIO_UART_GYRO_IOMUX_RX_FUNC);
+    DL_GPIO_initPeripheralOutputFunction(
+        GPIO_UART_BT_IOMUX_TX, GPIO_UART_BT_IOMUX_TX_FUNC);
+    DL_GPIO_initPeripheralInputFunction(
+        GPIO_UART_BT_IOMUX_RX, GPIO_UART_BT_IOMUX_RX_FUNC);
+
+    DL_GPIO_initDigitalInputFeatures(GRP_GRAY_OUT_OUT_IOMUX,
+		 DL_GPIO_INVERSION_DISABLE, DL_GPIO_RESISTOR_NONE,
+		 DL_GPIO_HYSTERESIS_DISABLE, DL_GPIO_WAKEUP_DISABLE);
+
+    DL_GPIO_initDigitalOutput(GRP_GRAY_AD0_IOMUX);
+
+    DL_GPIO_initDigitalOutput(GRP_GRAY_AD1_IOMUX);
+
+    DL_GPIO_initDigitalOutput(GRP_GRAY_AD2_IOMUX);
+
+    DL_GPIO_initDigitalInputFeatures(GRP_ENCODER_LEFT_A_IOMUX,
+		 DL_GPIO_INVERSION_DISABLE, DL_GPIO_RESISTOR_NONE,
+		 DL_GPIO_HYSTERESIS_DISABLE, DL_GPIO_WAKEUP_DISABLE);
+
+    DL_GPIO_initDigitalInputFeatures(GRP_ENCODER_LEFT_B_IOMUX,
+		 DL_GPIO_INVERSION_DISABLE, DL_GPIO_RESISTOR_NONE,
+		 DL_GPIO_HYSTERESIS_DISABLE, DL_GPIO_WAKEUP_DISABLE);
+
+    DL_GPIO_initDigitalInputFeatures(GRP_ENCODER_RIGHT_A_IOMUX,
+		 DL_GPIO_INVERSION_DISABLE, DL_GPIO_RESISTOR_NONE,
+		 DL_GPIO_HYSTERESIS_DISABLE, DL_GPIO_WAKEUP_DISABLE);
+
+    DL_GPIO_initDigitalInputFeatures(GRP_ENCODER_RIGHT_B_IOMUX,
+		 DL_GPIO_INVERSION_DISABLE, DL_GPIO_RESISTOR_NONE,
+		 DL_GPIO_HYSTERESIS_DISABLE, DL_GPIO_WAKEUP_DISABLE);
+
+    DL_GPIO_clearPins(GPIOA, GRP_GRAY_AD1_PIN |
+		GRP_GRAY_AD2_PIN);
+    DL_GPIO_enableOutput(GPIOA, GRP_GRAY_AD1_PIN |
+		GRP_GRAY_AD2_PIN);
+    DL_GPIO_clearPins(GPIOB, GRP_GRAY_AD0_PIN);
+    DL_GPIO_enableOutput(GPIOB, GRP_GRAY_AD0_PIN);
+    DL_GPIO_setLowerPinsPolarity(GPIOB, DL_GPIO_PIN_11_EDGE_RISE_FALL |
+		DL_GPIO_PIN_14_EDGE_RISE_FALL);
+    DL_GPIO_setUpperPinsPolarity(GPIOB, DL_GPIO_PIN_23_EDGE_RISE_FALL |
+		DL_GPIO_PIN_27_EDGE_RISE_FALL);
+    DL_GPIO_setLowerPinsInputFilter(GPIOB, DL_GPIO_PIN_11_INPUT_FILTER_3_CYCLES |
+		DL_GPIO_PIN_14_INPUT_FILTER_3_CYCLES);
+    DL_GPIO_setUpperPinsInputFilter(GPIOB, DL_GPIO_PIN_23_INPUT_FILTER_3_CYCLES |
+		DL_GPIO_PIN_27_INPUT_FILTER_3_CYCLES);
+    DL_GPIO_clearInterruptStatus(GPIOB, GRP_ENCODER_LEFT_A_PIN |
+		GRP_ENCODER_LEFT_B_PIN |
+		GRP_ENCODER_RIGHT_A_PIN |
+		GRP_ENCODER_RIGHT_B_PIN);
+    DL_GPIO_enableInterrupt(GPIOB, GRP_ENCODER_LEFT_A_PIN |
+		GRP_ENCODER_LEFT_B_PIN |
+		GRP_ENCODER_RIGHT_A_PIN |
+		GRP_ENCODER_RIGHT_B_PIN);
+
+}
+
+
+SYSCONFIG_WEAK void SYSCFG_DL_SYSCTL_init(void)
+{
+
+	//Low Power Mode is configured to be SLEEP0
+    DL_SYSCTL_setBORThreshold(DL_SYSCTL_BOR_THRESHOLD_LEVEL_0);
+
+    DL_SYSCTL_setSYSOSCFreq(DL_SYSCTL_SYSOSC_FREQ_BASE);
+    /* Set default configuration */
+    DL_SYSCTL_disableHFXT();
+    DL_SYSCTL_disableSYSPLL();
+    DL_SYSCTL_setULPCLKDivider(DL_SYSCTL_ULPCLK_DIV_1);
+    DL_SYSCTL_setMCLKDivider(DL_SYSCTL_MCLK_DIVIDER_DISABLE);
+
+}
+
+
+/*
+ * Timer clock configuration to be sourced by  / 1 (32000000 Hz)
+ * timerClkFreq = (timerClkSrc / (timerClkDivRatio * (timerClkPrescale + 1)))
+ *   32000000 Hz = 32000000 Hz / (1 * (0 + 1))
+ */
+static const DL_TimerA_ClockConfig gPWM_RIGHTClockConfig = {
+    .clockSel = DL_TIMER_CLOCK_BUSCLK,
+    .divideRatio = DL_TIMER_CLOCK_DIVIDE_1,
+    .prescale = 0U
+};
+
+static const DL_TimerA_PWMConfig gPWM_RIGHTConfig = {
+    .pwmMode = DL_TIMER_PWM_MODE_EDGE_ALIGN,
+    .period = 3200,
+    .isTimerWithFourCC = true,
+    .startTimer = DL_TIMER_STOP,
+};
+
+SYSCONFIG_WEAK void SYSCFG_DL_PWM_RIGHT_init(void) {
+
+    DL_TimerA_setClockConfig(
+        PWM_RIGHT_INST, (DL_TimerA_ClockConfig *) &gPWM_RIGHTClockConfig);
+
+    DL_TimerA_initPWMMode(
+        PWM_RIGHT_INST, (DL_TimerA_PWMConfig *) &gPWM_RIGHTConfig);
+
+    // Set Counter control to the smallest CC index being used
+    DL_TimerA_setCounterControl(PWM_RIGHT_INST,DL_TIMER_CZC_CCCTL0_ZCOND,DL_TIMER_CAC_CCCTL0_ACOND,DL_TIMER_CLC_CCCTL0_LCOND);
+
+    DL_TimerA_setCaptureCompareOutCtl(PWM_RIGHT_INST, DL_TIMER_CC_OCTL_INIT_VAL_LOW,
+		DL_TIMER_CC_OCTL_INV_OUT_DISABLED, DL_TIMER_CC_OCTL_SRC_FUNCVAL,
+		DL_TIMERA_CAPTURE_COMPARE_0_INDEX);
+
+    DL_TimerA_setCaptCompUpdateMethod(PWM_RIGHT_INST, DL_TIMER_CC_UPDATE_METHOD_IMMEDIATE, DL_TIMERA_CAPTURE_COMPARE_0_INDEX);
+    DL_TimerA_setCaptureCompareValue(PWM_RIGHT_INST, 3200, DL_TIMER_CC_0_INDEX);
+
+    DL_TimerA_setCaptureCompareOutCtl(PWM_RIGHT_INST, DL_TIMER_CC_OCTL_INIT_VAL_LOW,
+		DL_TIMER_CC_OCTL_INV_OUT_DISABLED, DL_TIMER_CC_OCTL_SRC_FUNCVAL,
+		DL_TIMERA_CAPTURE_COMPARE_1_INDEX);
+
+    DL_TimerA_setCaptCompUpdateMethod(PWM_RIGHT_INST, DL_TIMER_CC_UPDATE_METHOD_IMMEDIATE, DL_TIMERA_CAPTURE_COMPARE_1_INDEX);
+    DL_TimerA_setCaptureCompareValue(PWM_RIGHT_INST, 3200, DL_TIMER_CC_1_INDEX);
+
+    DL_TimerA_enableClock(PWM_RIGHT_INST);
+
+
+    
+    DL_TimerA_setCCPDirection(PWM_RIGHT_INST , DL_TIMER_CC0_OUTPUT | DL_TIMER_CC1_OUTPUT );
+
+
+}
+/*
+ * Timer clock configuration to be sourced by  / 1 (32000000 Hz)
+ * timerClkFreq = (timerClkSrc / (timerClkDivRatio * (timerClkPrescale + 1)))
+ *   32000000 Hz = 32000000 Hz / (1 * (0 + 1))
+ */
+static const DL_TimerA_ClockConfig gPWM_LEFTClockConfig = {
+    .clockSel = DL_TIMER_CLOCK_BUSCLK,
+    .divideRatio = DL_TIMER_CLOCK_DIVIDE_1,
+    .prescale = 0U
+};
+
+static const DL_TimerA_PWMConfig gPWM_LEFTConfig = {
+    .pwmMode = DL_TIMER_PWM_MODE_EDGE_ALIGN,
+    .period = 3200,
+    .isTimerWithFourCC = false,
+    .startTimer = DL_TIMER_STOP,
+};
+
+SYSCONFIG_WEAK void SYSCFG_DL_PWM_LEFT_init(void) {
+
+    DL_TimerA_setClockConfig(
+        PWM_LEFT_INST, (DL_TimerA_ClockConfig *) &gPWM_LEFTClockConfig);
+
+    DL_TimerA_initPWMMode(
+        PWM_LEFT_INST, (DL_TimerA_PWMConfig *) &gPWM_LEFTConfig);
+
+    // Set Counter control to the smallest CC index being used
+    DL_TimerA_setCounterControl(PWM_LEFT_INST,DL_TIMER_CZC_CCCTL0_ZCOND,DL_TIMER_CAC_CCCTL0_ACOND,DL_TIMER_CLC_CCCTL0_LCOND);
+
+    DL_TimerA_setCaptureCompareOutCtl(PWM_LEFT_INST, DL_TIMER_CC_OCTL_INIT_VAL_LOW,
+		DL_TIMER_CC_OCTL_INV_OUT_DISABLED, DL_TIMER_CC_OCTL_SRC_FUNCVAL,
+		DL_TIMERA_CAPTURE_COMPARE_0_INDEX);
+
+    DL_TimerA_setCaptCompUpdateMethod(PWM_LEFT_INST, DL_TIMER_CC_UPDATE_METHOD_IMMEDIATE, DL_TIMERA_CAPTURE_COMPARE_0_INDEX);
+    DL_TimerA_setCaptureCompareValue(PWM_LEFT_INST, 3200, DL_TIMER_CC_0_INDEX);
+
+    DL_TimerA_setCaptureCompareOutCtl(PWM_LEFT_INST, DL_TIMER_CC_OCTL_INIT_VAL_LOW,
+		DL_TIMER_CC_OCTL_INV_OUT_DISABLED, DL_TIMER_CC_OCTL_SRC_FUNCVAL,
+		DL_TIMERA_CAPTURE_COMPARE_1_INDEX);
+
+    DL_TimerA_setCaptCompUpdateMethod(PWM_LEFT_INST, DL_TIMER_CC_UPDATE_METHOD_IMMEDIATE, DL_TIMERA_CAPTURE_COMPARE_1_INDEX);
+    DL_TimerA_setCaptureCompareValue(PWM_LEFT_INST, 3200, DL_TIMER_CC_1_INDEX);
+
+    DL_TimerA_enableClock(PWM_LEFT_INST);
+
+
+    
+    DL_TimerA_setCCPDirection(PWM_LEFT_INST , DL_TIMER_CC0_OUTPUT | DL_TIMER_CC1_OUTPUT );
+
+
+}
+
+
+
+/*
+ * Timer clock configuration to be sourced by BUSCLK /  (32000000 Hz)
+ * timerClkFreq = (timerClkSrc / (timerClkDivRatio * (timerClkPrescale + 1)))
+ *   32000000 Hz = 32000000 Hz / (1 * (0 + 1))
+ */
+static const DL_TimerG_ClockConfig gTIMER_0ClockConfig = {
+    .clockSel    = DL_TIMER_CLOCK_BUSCLK,
+    .divideRatio = DL_TIMER_CLOCK_DIVIDE_1,
+    .prescale    = 0U,
+};
+
+/*
+ * Timer load value (where the counter starts from) is calculated as (timerPeriod * timerClockFreq) - 1
+ * TIMER_0_INST_LOAD_VALUE = (0.5 ms * 32000000 Hz) - 1
+ */
+static const DL_TimerG_TimerConfig gTIMER_0TimerConfig = {
+    .period     = TIMER_0_INST_LOAD_VALUE,
+    .timerMode  = DL_TIMER_TIMER_MODE_PERIODIC,
+    .startTimer = DL_TIMER_STOP,
+};
+
+SYSCONFIG_WEAK void SYSCFG_DL_TIMER_0_init(void) {
+
+    DL_TimerG_setClockConfig(TIMER_0_INST,
+        (DL_TimerG_ClockConfig *) &gTIMER_0ClockConfig);
+
+    DL_TimerG_initTimerMode(TIMER_0_INST,
+        (DL_TimerG_TimerConfig *) &gTIMER_0TimerConfig);
+    DL_TimerG_enableInterrupt(TIMER_0_INST , DL_TIMERG_INTERRUPT_ZERO_EVENT);
+    DL_TimerG_enableClock(TIMER_0_INST);
+
+
+
+
+
+}
+
+
+static const DL_UART_Main_ClockConfig gUART_GYROClockConfig = {
+    .clockSel    = DL_UART_MAIN_CLOCK_BUSCLK,
+    .divideRatio = DL_UART_MAIN_CLOCK_DIVIDE_RATIO_1
+};
+
+static const DL_UART_Main_Config gUART_GYROConfig = {
+    .mode        = DL_UART_MAIN_MODE_NORMAL,
+    .direction   = DL_UART_MAIN_DIRECTION_TX_RX,
+    .flowControl = DL_UART_MAIN_FLOW_CONTROL_NONE,
+    .parity      = DL_UART_MAIN_PARITY_NONE,
+    .wordLength  = DL_UART_MAIN_WORD_LENGTH_8_BITS,
+    .stopBits    = DL_UART_MAIN_STOP_BITS_ONE
+};
+
+SYSCONFIG_WEAK void SYSCFG_DL_UART_GYRO_init(void)
+{
+    DL_UART_Main_setClockConfig(UART_GYRO_INST, (DL_UART_Main_ClockConfig *) &gUART_GYROClockConfig);
+
+    DL_UART_Main_init(UART_GYRO_INST, (DL_UART_Main_Config *) &gUART_GYROConfig);
+    /*
+     * Configure baud rate by setting oversampling and baud rate divisors.
+     *  Target baud rate: 115200
+     *  Actual baud rate: 115211.52
+     */
+    DL_UART_Main_setOversampling(UART_GYRO_INST, DL_UART_OVERSAMPLING_RATE_16X);
+    DL_UART_Main_setBaudRateDivisor(UART_GYRO_INST, UART_GYRO_IBRD_32_MHZ_115200_BAUD, UART_GYRO_FBRD_32_MHZ_115200_BAUD);
+
+
+    /* Configure Interrupts */
+    DL_UART_Main_enableInterrupt(UART_GYRO_INST,
+                                 DL_UART_MAIN_INTERRUPT_RX);
+
+
+    DL_UART_Main_enable(UART_GYRO_INST);
+}
+static const DL_UART_Main_ClockConfig gUART_BTClockConfig = {
+    .clockSel    = DL_UART_MAIN_CLOCK_BUSCLK,
+    .divideRatio = DL_UART_MAIN_CLOCK_DIVIDE_RATIO_1
+};
+
+static const DL_UART_Main_Config gUART_BTConfig = {
+    .mode        = DL_UART_MAIN_MODE_NORMAL,
+    .direction   = DL_UART_MAIN_DIRECTION_TX_RX,
+    .flowControl = DL_UART_MAIN_FLOW_CONTROL_NONE,
+    .parity      = DL_UART_MAIN_PARITY_NONE,
+    .wordLength  = DL_UART_MAIN_WORD_LENGTH_8_BITS,
+    .stopBits    = DL_UART_MAIN_STOP_BITS_ONE
+};
+
+SYSCONFIG_WEAK void SYSCFG_DL_UART_BT_init(void)
+{
+    DL_UART_Main_setClockConfig(UART_BT_INST, (DL_UART_Main_ClockConfig *) &gUART_BTClockConfig);
+
+    DL_UART_Main_init(UART_BT_INST, (DL_UART_Main_Config *) &gUART_BTConfig);
+    /*
+     * Configure baud rate by setting oversampling and baud rate divisors.
+     *  Target baud rate: 115200
+     *  Actual baud rate: 115211.52
+     */
+    DL_UART_Main_setOversampling(UART_BT_INST, DL_UART_OVERSAMPLING_RATE_16X);
+    DL_UART_Main_setBaudRateDivisor(UART_BT_INST, UART_BT_IBRD_32_MHZ_115200_BAUD, UART_BT_FBRD_32_MHZ_115200_BAUD);
+
+
+
+    DL_UART_Main_enable(UART_BT_INST);
+}
+
---
+++ 2025xiaochexunji/Debug/ti_msp_dl_config.h
@@ -0,0 +1,225 @@
+/*
+ * Copyright (c) 2023, Texas Instruments Incorporated - http://www.ti.com
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * *  Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * *  Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * *  Neither the name of Texas Instruments Incorporated nor the names of
+ *    its contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+ * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/*
+ *  ============ ti_msp_dl_config.h =============
+ *  Configured MSPM0 DriverLib module declarations
+ *
+ *  DO NOT EDIT - This file is generated for the MSPM0G350X
+ *  by the SysConfig tool.
+ */
+#ifndef ti_msp_dl_config_h
+#define ti_msp_dl_config_h
+
+#define CONFIG_MSPM0G350X
+#define CONFIG_MSPM0G3507
+
+#if defined(__ti_version__) || defined(__TI_COMPILER_VERSION__)
+#define SYSCONFIG_WEAK __attribute__((weak))
+#elif defined(__IAR_SYSTEMS_ICC__)
+#define SYSCONFIG_WEAK __weak
+#elif defined(__GNUC__)
+#define SYSCONFIG_WEAK __attribute__((weak))
+#endif
+
+#include <ti/devices/msp/msp.h>
+#include <ti/driverlib/driverlib.h>
+#include <ti/driverlib/m0p/dl_core.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/*
+ *  ======== SYSCFG_DL_init ========
+ *  Perform all required MSP DL initialization
+ *
+ *  This function should be called once at a point before any use of
+ *  MSP DL.
+ */
+
+
+/* clang-format off */
+
+#define POWER_STARTUP_DELAY                                                (16)
+
+
+#define CPUCLK_FREQ                                                     32000000
+
+
+
+/* Defines for PWM_RIGHT */
+#define PWM_RIGHT_INST                                                     TIMA0
+#define PWM_RIGHT_INST_IRQHandler                               TIMA0_IRQHandler
+#define PWM_RIGHT_INST_INT_IRQN                                 (TIMA0_INT_IRQn)
+#define PWM_RIGHT_INST_CLK_FREQ                                         32000000
+/* GPIO defines for channel 0 */
+#define GPIO_PWM_RIGHT_C0_PORT                                             GPIOA
+#define GPIO_PWM_RIGHT_C0_PIN                                      DL_GPIO_PIN_0
+#define GPIO_PWM_RIGHT_C0_IOMUX                                   (IOMUX_PINCM1)
+#define GPIO_PWM_RIGHT_C0_IOMUX_FUNC                  IOMUX_PINCM1_PF_TIMA0_CCP0
+#define GPIO_PWM_RIGHT_C0_IDX                                DL_TIMER_CC_0_INDEX
+/* GPIO defines for channel 1 */
+#define GPIO_PWM_RIGHT_C1_PORT                                             GPIOA
+#define GPIO_PWM_RIGHT_C1_PIN                                      DL_GPIO_PIN_1
+#define GPIO_PWM_RIGHT_C1_IOMUX                                   (IOMUX_PINCM2)
+#define GPIO_PWM_RIGHT_C1_IOMUX_FUNC                  IOMUX_PINCM2_PF_TIMA0_CCP1
+#define GPIO_PWM_RIGHT_C1_IDX                                DL_TIMER_CC_1_INDEX
+
+/* Defines for PWM_LEFT */
+#define PWM_LEFT_INST                                                      TIMA1
+#define PWM_LEFT_INST_IRQHandler                                TIMA1_IRQHandler
+#define PWM_LEFT_INST_INT_IRQN                                  (TIMA1_INT_IRQn)
+#define PWM_LEFT_INST_CLK_FREQ                                          32000000
+/* GPIO defines for channel 0 */
+#define GPIO_PWM_LEFT_C0_PORT                                              GPIOA
+#define GPIO_PWM_LEFT_C0_PIN                                      DL_GPIO_PIN_10
+#define GPIO_PWM_LEFT_C0_IOMUX                                   (IOMUX_PINCM21)
+#define GPIO_PWM_LEFT_C0_IOMUX_FUNC                  IOMUX_PINCM21_PF_TIMA1_CCP0
+#define GPIO_PWM_LEFT_C0_IDX                                 DL_TIMER_CC_0_INDEX
+/* GPIO defines for channel 1 */
+#define GPIO_PWM_LEFT_C1_PORT                                              GPIOA
+#define GPIO_PWM_LEFT_C1_PIN                                      DL_GPIO_PIN_11
+#define GPIO_PWM_LEFT_C1_IOMUX                                   (IOMUX_PINCM22)
+#define GPIO_PWM_LEFT_C1_IOMUX_FUNC                  IOMUX_PINCM22_PF_TIMA1_CCP1
+#define GPIO_PWM_LEFT_C1_IDX                                 DL_TIMER_CC_1_INDEX
+
+
+
+/* Defines for TIMER_0 */
+#define TIMER_0_INST                                                     (TIMG0)
+#define TIMER_0_INST_IRQHandler                                 TIMG0_IRQHandler
+#define TIMER_0_INST_INT_IRQN                                   (TIMG0_INT_IRQn)
+#define TIMER_0_INST_LOAD_VALUE                                         (15999U)
+
+
+
+/* Defines for UART_GYRO */
+#define UART_GYRO_INST                                                     UART0
+#define UART_GYRO_INST_FREQUENCY                                        32000000
+#define UART_GYRO_INST_IRQHandler                               UART0_IRQHandler
+#define UART_GYRO_INST_INT_IRQN                                   UART0_INT_IRQn
+#define GPIO_UART_GYRO_RX_PORT                                             GPIOA
+#define GPIO_UART_GYRO_TX_PORT                                             GPIOA
+#define GPIO_UART_GYRO_RX_PIN                                     DL_GPIO_PIN_31
+#define GPIO_UART_GYRO_TX_PIN                                     DL_GPIO_PIN_28
+#define GPIO_UART_GYRO_IOMUX_RX                                   (IOMUX_PINCM6)
+#define GPIO_UART_GYRO_IOMUX_TX                                   (IOMUX_PINCM3)
+#define GPIO_UART_GYRO_IOMUX_RX_FUNC                    IOMUX_PINCM6_PF_UART0_RX
+#define GPIO_UART_GYRO_IOMUX_TX_FUNC                    IOMUX_PINCM3_PF_UART0_TX
+#define UART_GYRO_BAUD_RATE                                             (115200)
+#define UART_GYRO_IBRD_32_MHZ_115200_BAUD                                   (17)
+#define UART_GYRO_FBRD_32_MHZ_115200_BAUD                                   (23)
+/* Defines for UART_BT */
+#define UART_BT_INST                                                       UART2
+#define UART_BT_INST_FREQUENCY                                          32000000
+#define UART_BT_INST_IRQHandler                                 UART2_IRQHandler
+#define UART_BT_INST_INT_IRQN                                     UART2_INT_IRQn
+#define GPIO_UART_BT_RX_PORT                                               GPIOA
+#define GPIO_UART_BT_TX_PORT                                               GPIOA
+#define GPIO_UART_BT_RX_PIN                                       DL_GPIO_PIN_22
+#define GPIO_UART_BT_TX_PIN                                       DL_GPIO_PIN_21
+#define GPIO_UART_BT_IOMUX_RX                                    (IOMUX_PINCM47)
+#define GPIO_UART_BT_IOMUX_TX                                    (IOMUX_PINCM46)
+#define GPIO_UART_BT_IOMUX_RX_FUNC                     IOMUX_PINCM47_PF_UART2_RX
+#define GPIO_UART_BT_IOMUX_TX_FUNC                     IOMUX_PINCM46_PF_UART2_TX
+#define UART_BT_BAUD_RATE                                               (115200)
+#define UART_BT_IBRD_32_MHZ_115200_BAUD                                     (17)
+#define UART_BT_FBRD_32_MHZ_115200_BAUD                                     (23)
+
+
+
+
+
+/* Port definition for Pin Group GRP_GRAY_OUT */
+#define GRP_GRAY_OUT_PORT                                                (GPIOB)
+
+/* Defines for OUT: GPIOB.20 with pinCMx 48 on package pin 19 */
+#define GRP_GRAY_OUT_OUT_PIN                                    (DL_GPIO_PIN_20)
+#define GRP_GRAY_OUT_OUT_IOMUX                                   (IOMUX_PINCM48)
+/* Defines for AD0: GPIOB.25 with pinCMx 56 on package pin 27 */
+#define GRP_GRAY_AD0_PORT                                                (GPIOB)
+#define GRP_GRAY_AD0_PIN                                        (DL_GPIO_PIN_25)
+#define GRP_GRAY_AD0_IOMUX                                       (IOMUX_PINCM56)
+/* Defines for AD1: GPIOA.25 with pinCMx 55 on package pin 26 */
+#define GRP_GRAY_AD1_PORT                                                (GPIOA)
+#define GRP_GRAY_AD1_PIN                                        (DL_GPIO_PIN_25)
+#define GRP_GRAY_AD1_IOMUX                                       (IOMUX_PINCM55)
+/* Defines for AD2: GPIOA.27 with pinCMx 60 on package pin 31 */
+#define GRP_GRAY_AD2_PORT                                                (GPIOA)
+#define GRP_GRAY_AD2_PIN                                        (DL_GPIO_PIN_27)
+#define GRP_GRAY_AD2_IOMUX                                       (IOMUX_PINCM60)
+/* Port definition for Pin Group GRP_ENCODER */
+#define GRP_ENCODER_PORT                                                 (GPIOB)
+
+/* Defines for LEFT_A: GPIOB.23 with pinCMx 51 on package pin 22 */
+// pins affected by this interrupt request:["LEFT_A","LEFT_B","RIGHT_A","RIGHT_B"]
+#define GRP_ENCODER_INT_IRQN                                    (GPIOB_INT_IRQn)
+#define GRP_ENCODER_INT_IIDX                    (DL_INTERRUPT_GROUP1_IIDX_GPIOB)
+#define GRP_ENCODER_LEFT_A_IIDX                             (DL_GPIO_IIDX_DIO23)
+#define GRP_ENCODER_LEFT_A_PIN                                  (DL_GPIO_PIN_23)
+#define GRP_ENCODER_LEFT_A_IOMUX                                 (IOMUX_PINCM51)
+/* Defines for LEFT_B: GPIOB.27 with pinCMx 58 on package pin 29 */
+#define GRP_ENCODER_LEFT_B_IIDX                             (DL_GPIO_IIDX_DIO27)
+#define GRP_ENCODER_LEFT_B_PIN                                  (DL_GPIO_PIN_27)
+#define GRP_ENCODER_LEFT_B_IOMUX                                 (IOMUX_PINCM58)
+/* Defines for RIGHT_A: GPIOB.11 with pinCMx 28 on package pin 63 */
+#define GRP_ENCODER_RIGHT_A_IIDX                            (DL_GPIO_IIDX_DIO11)
+#define GRP_ENCODER_RIGHT_A_PIN                                 (DL_GPIO_PIN_11)
+#define GRP_ENCODER_RIGHT_A_IOMUX                                (IOMUX_PINCM28)
+/* Defines for RIGHT_B: GPIOB.14 with pinCMx 31 on package pin 2 */
+#define GRP_ENCODER_RIGHT_B_IIDX                            (DL_GPIO_IIDX_DIO14)
+#define GRP_ENCODER_RIGHT_B_PIN                                 (DL_GPIO_PIN_14)
+#define GRP_ENCODER_RIGHT_B_IOMUX                                (IOMUX_PINCM31)
+
+
+/* clang-format on */
+
+void SYSCFG_DL_init(void);
+void SYSCFG_DL_initPower(void);
+void SYSCFG_DL_GPIO_init(void);
+void SYSCFG_DL_SYSCTL_init(void);
+void SYSCFG_DL_PWM_RIGHT_init(void);
+void SYSCFG_DL_PWM_LEFT_init(void);
+void SYSCFG_DL_TIMER_0_init(void);
+void SYSCFG_DL_UART_GYRO_init(void);
+void SYSCFG_DL_UART_BT_init(void);
+
+
+bool SYSCFG_DL_saveConfiguration(void);
+bool SYSCFG_DL_restoreConfiguration(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* ti_msp_dl_config_h */
